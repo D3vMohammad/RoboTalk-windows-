43ab8be44248a5838f6ea48c4147d20be809e27d
Mohammad Ahmad <112339868+D3vMohammad@users.noreply.github.com>
2023-12-21 12:19:58 +0530

    Update text_to_speech.py
    fixed some bugs

--- text_to_speech.py
+++ text_to_speech.py
@@ -6,12 +6,12 @@ def text_to_speech(text):
     engine.runAndWait()
 
 if __name__ == "__main__":
-    print("welcome to robo chat, to quite write 'q' alone, and press enter :)")
+    print("welcome to robo chat, to quite write 'bye robo' alone, and press enter :)")
     
     while True:
         input_text = input("Enter the text you want to convert to speech: ")
 
-        if input_text == "by robo":
+        if input_text == "bye robo":
             print("Thanks for using robo talk")
             break
         text_to_speech(input_text)
